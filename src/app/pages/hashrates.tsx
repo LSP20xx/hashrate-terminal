d05d172c7969dc7a5fe955ae94c4fc8f617ab796
Lautaro Perrotti <lautaroperrotti@gmail.com>
2024-01-09 05:28:15 -0300

    third

---
+++ src/app/pages/hashrates.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HashrateComponent from "../components/HashrateComponent";
+
+const HashratesPage = () => {
+  return (
+    <div>
+      <h1>Hashrate Monitoring</h1>
+      <HashrateComponent />
+    </div>
+  );
+};
+
+export default HashratesPage;
